7a8db6b981fc2527692598311f8e69876513a6ac
digitaldotmy <83358304+digitaldotmy@users.noreply.github.com>
2023-01-03 16:18:22 +0800

    Update dailymotion_m3ugrabber.py

--- scripts/dailymotion_m3ugrabber.py
+++ scripts/dailymotion_m3ugrabber.py
@@ -23,7 +23,7 @@ if len(sys.argv) == 2:
                 'https' : sys.argv[1]
               }
 
-na = 'https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u'
+na = 'https://raw.githubusercontent.com/digitaldotmy/YoutubeLive/main/assets/Interrupted.m3u'
 def grab(line):
     try:
         _id = line.split('/')[4]
